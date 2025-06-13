f64ea05f30dddb3075c9f13cbb84360a98385f86
potato-pzy <21613@yenepoya.edu.in>
2025-06-13 10:29:33 +0530

    4 commut

--- build.sh
+++ build.sh
@@ -2,10 +2,5 @@
 # Exit on error
 set -o errexit
 
-# Install system dependencies
-apt-get update -y
-apt-get install -y tesseract-ocr
-apt-get install -y tesseract-ocr-eng
-
 # Install Python dependencies
 pip install -r requirements.txt 
